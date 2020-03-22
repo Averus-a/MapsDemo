0364c7de38d5937296c2bd7582935d008fbac96a
Artem Saikin <art.saikin@gmail.com>
2020-03-22 13:32:31 +0400

    Add main window with base map

---
+++ MapsDemo/MapsDemo/ViewModels/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace MapsDemo.ViewModels
+{
+    using Catel.MVVM;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+
+        }
+
+        public int MyProperty { get; set; }
+}
+}
